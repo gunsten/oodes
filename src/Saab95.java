6dd2422af4ac2cedb70ef73abb9c54ff5de3684f
gunsten <gunnar.stenlund@gmail.com>
2019-02-10 17:24:24 +0100

    Finishes Lab1A
    Comments public and protected members of Saab95 and Volvo240

--- src/Saab95.java
+++ src/Saab95.java
@@ -1,24 +1,38 @@
 import java.awt.*;
 
+/**
+ * A car of the Volvo240 model
+ */
 public class Saab95 extends Car{
 
     private boolean turboOn;
 
-    
+    /**
+     * Creates a Saab95 object
+     */
     public Saab95(){
         super(2, 125, Color.red, "Saab95");
 	    turboOn = false;
     }
 
+    /**
+     * Activates the turbo
+     */
     public void setTurboOn(){
 	    turboOn = true;
     }
 
+    /**
+     * Deactivates the turbo
+     */
     public void setTurboOff(){
 	    turboOn = false;
     }
-    
-    public double speedFactor(){
+
+    /**
+     * @return the calculated speed factor of this specific car model
+     */
+    protected double speedFactor(){
         double turbo = 1;
         if(turboOn) turbo = 1.3;
         return getEnginePower() * 0.01 * turbo;
--- src/Volvo240.java
+++ src/Volvo240.java
@@ -1,14 +1,26 @@
 import java.awt.*;
 
+/**
+ * A car of the Volvo240 model
+ */
 public class Volvo240 extends Car{
 
+    /**
+     * The trim factor of the Volvo240, effecting the speed change of the car
+     */
     public final static double trimFactor = 1.25;
-    
+
+    /**
+     * Creates a Volvo240 object
+     */
     public Volvo240(){
         super(4, 100, Color.black, "Volvo240");
     }
-    
-    public double speedFactor(){
+
+    /**
+     * @return the calculated speed factor of this specific car model
+     */
+    protected double speedFactor(){
         return getEnginePower() * 0.01 * trimFactor;
     }
 }
