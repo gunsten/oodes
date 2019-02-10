6ef162f0a3dc1082da2886484a6f21fccfc9b650
gunsten <gunnar.stenlund@gmail.com>
2019-02-10 18:35:44 +0100

    Implements Scania tests

---
+++ test/TestScania.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestScania {
+    @Test
+    public void testDumper() {
+        Scania scania = new Scania();
+        for (int i=0;i<20;i++) {
+            scania.raisePlatform();
+            assertTrue(scania.getPlatformAngle() <= scania.DUMPERMAXANGLE);
+        }
+        for (int i=0;i<20;i++) {
+            scania.lowerPlatform();
+            assertTrue(scania.getPlatformAngle() >= scania.DUMPERMINANGLE);
+        }
+
+        scania.gas(1);
+        boolean except1 = false;
+        try {
+            scania.raisePlatform();
+        } catch (PlatformException e) {
+            except1 = true;
+        }
+        assertTrue(except1);
+
+        scania.stopEngine();
+        scania.raisePlatform();
+        boolean except2 = false;
+        try {
+            scania.gas(1);
+        } catch (PlatformException e) {
+            except2 = true;
+        }
+        assertTrue(except2);
+    }
+}
