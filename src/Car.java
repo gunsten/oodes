6e7d9c29b4d424e89dd792e15ce377ce842f3976
gunsten <gunnar.stenlund@gmail.com>
2019-02-10 15:58:03 +0100

    Implements Movable methods for Car

--- src/Car.java
+++ src/Car.java
@@ -1,17 +1,25 @@
 import java.awt.*;
+import java.awt.geom.Point2D;
 
-public abstract class Car {
+public abstract class Car implements Movable{
+    public static final int TURNINGDEGREES = 10;
+
     private int nrDoors; // Number of doors on the car
     private double enginePower; // Engine power of the car
     private double currentSpeed; // The current speed of the car
     private Color color; // Color of the car
     private String modelName; // The car model name
 
+    private Point2D.Double position;
+    private double direction;
+
     public Car(int nrDoors, double enginePower, Color color, String modelName) {
         this.nrDoors = nrDoors;
         this.enginePower = enginePower;
         this.currentSpeed = 0;
         this.color = color;
+        position = new Point2D.Double(0,0);
+        direction = 0;
         stopEngine();
     }
 
@@ -49,6 +57,28 @@ public abstract class Car {
     public abstract void incrementSpeed(double amount);
 
     public abstract void decrementSpeed(double amount);
+
+    @Override
+    public void move() {
+        double newx = position.x + currentSpeed * Math.cos(direction);
+        double newy = position.y + currentSpeed * Math.sin(direction);
+    }
+
+    @Override
+    public void turnLeft() {
+        direction -= Math.toRadians(TURNINGDEGREES);
+        while(direction < 0) {
+            direction += 2*Math.PI;
+        }
+    }
+
+    @Override
+    public void turnRight() {
+        direction += Math.toRadians(TURNINGDEGREES);
+        while(direction > 2*Math.PI) {
+            direction -= 2*Math.PI;
+        }
+    }
 
     // TODO fix this method according to lab pm
     public void gas(double amount){
