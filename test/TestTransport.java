021ba2de15a9becf72053cf6445c03365de5b369
gunsten <gunnar.stenlund@gmail.com>
2019-02-11 12:33:00 +0100

    Implements complementary tests of Transport

--- test/TestTransport.java
+++ test/TestTransport.java
@@ -46,9 +46,89 @@ public class TestTransport {
         }
 
         tp.stopEngine();
+
+        boolean except3 = false;
+
+        try {
+            tp.unloadCar();
+        } catch (LoadException e) {
+            if (e.getMessage().contains("ramp is up"))
+                except3 = true;
+        }
+
+        assertTrue(except3);
+
         tp.lowerPlatform();
         tp.unloadCar();
 
         assertTrue(tp.distance(volvo) == 1d);
+        assertEquals(false, volvo.isLoaded());
+
+        Saab95 saab = new Saab95();
+
+        boolean except4 = false;
+
+        try {
+            tp.loadCar(saab);
+        } catch (LoadException e) {
+            if (e.getMessage().contains("too far away"))
+                except4 = true;
+        }
+
+        assertTrue(except4);
+        assertEquals(false, saab.isLoaded());
+    }
+
+    @Test
+    public void testConstructor() {
+        boolean except = false;
+        try {
+            new Transport(-1);
+        } catch (IllegalArgumentException e) {
+            except = true;
+        }
+        assertTrue(except);
+
+        except = false;
+        try {
+            new Transport(0);
+        } catch (IllegalArgumentException e) {
+            except = true;
+        }
+        assertTrue(except);
+    }
+
+    @Test
+    public void testEmpty() {
+        Transport tp = new Transport(1);
+        tp.lowerPlatform();
+
+        boolean except = false;
+
+        try {
+            tp.unloadCar();
+        } catch (LoadException e) {
+            if (e.getMessage().contains("empty"))
+                except = true;
+        }
+
+        assertTrue(except);
+    }
+
+    @Test
+    public void testFull() {
+        Transport tp = new Transport(1);
+        tp.lowerPlatform();
+        tp.loadCar(new Volvo240());
+        boolean except = false;
+
+        try {
+            tp.loadCar(new Saab95());
+        } catch (LoadException e) {
+            if (e.getMessage().contains("full"))
+                except = true;
+        }
+
+        assertTrue(except);
     }
 }
