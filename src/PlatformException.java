0532bcfb010d563a1ed884f6c0b7fa71319def97
gunsten <gunnar.stenlund@gmail.com>
2019-02-27 17:32:51 +0100

    Removes files moved to other packages

--- src/PlatformException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Exception thrown when trying to drive a Truck with unclosed platform, or trying to open a platform of a running Truck
- */
-public class PlatformException extends RuntimeException{
-    public PlatformException(String msg) {
-        super(msg);
-    }
-}
--- src/ReadableModel.java
+++
@@ -1,5 +0,0 @@
-import java.util.List;
-
-public interface ReadableModel<T extends Car> {
-    public List<T> get();
-}
