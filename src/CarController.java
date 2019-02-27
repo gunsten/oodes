32f5cf97562a99e03d0f12181a15f4bc5c31924b
gunsten <gunnar.stenlund@gmail.com>
2019-02-27 15:55:05 +0100

    Refactors CC

--- src/CarController.java
+++ src/CarController.java
@@ -1,8 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.Point2D;
-import java.util.ArrayList;
 
 /*
 * This class represents the Controller part in the MVC pattern.
@@ -19,74 +18,38 @@ public class CarController {
     // each step between delays.
     private Timer timer = new Timer(delay, new TimerListener());
 
-    // The frame that represents this instance View of the MVC pattern
-    CarView frame;
-    // A list of cars, modify if needed
-    ArrayList<Car> cars = new ArrayList<>();
+    Component updateableView;
+    IModel model;
 
+    public CarController(Component updateableView, IModel model) {
+        this.updateableView = updateableView;
+        this.model = model;
+    }
     //methods:
 
-    public static void main(String[] args) {
-        // Instance of this class
-        CarController cc = new CarController();
-
-        Car volvo = new Volvo240();
-        cc.cars.add(volvo);
-
-        Car saab = new Saab95();
-        saab.setPosition(new Point2D.Double(0,100d));
-        cc.cars.add(saab);
-        Car scania = new Scania();
-        scania.setPosition(new Point2D.Double(0, 200d));
-        cc.cars.add(scania);
-
-        // Start a new view and send a reference of self
-        cc.frame = new CarView("CarSim 1.0", cc);
-
-        // Add "tokens" to drawPanel
-        cc.frame.drawPanel.addCar(volvo, 0,0);
-        cc.frame.drawPanel.addCar(saab, 0,100);
-        cc.frame.drawPanel.addCar(scania, 0,200);
-
-        // Start the timer
-        cc.timer.start();
-    }
 
     public void turbo(boolean on) {
-        for (Car car : cars) {
-            if (car instanceof Saab95) {
-                if (on)
-                    ((Saab95) car).setTurboOn();
-                else
-                    ((Saab95) car).setTurboOff();
-            }
+        if (on) {
+            model.turbosOn();
+        } else {
+            model.turbosOff();
         }
     }
 
     public void liftBed() {
-        for (Car car : cars) {
-            if (car instanceof Truck) {
-                ((Truck) car).raisePlatform();
-            }
-        }
+        model.liftBeds();
     }
 
     public void lowerBed() {
-        for (Car car : cars) {
-            if (car instanceof Truck) {
-                ((Truck) car).lowerPlatform();
-            }
-        }
+        model.lowerBeds();
     }
 
     public void startCars() {
-        for (Car car : cars)
-            car.startEngine();
+        model.startCars();
     }
 
     public void stopCars() {
-        for (Car car : cars)
-            car.stopEngine();
+        model.stopCars();
     }
 
     /* Each step the TimerListener moves all the cars in the list and tells the
@@ -94,37 +57,18 @@ public class CarController {
     * */
     private class TimerListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            for (Car car : cars) {
-                car.move();
-                int x = (int) Math.round(car.getPosition().getX());
-                int y = (int) Math.round(car.getPosition().getY());
-
-                // Change car direction at edge of panel
-                if(x < 0 || x + 100 > frame.drawPanel.getWidth()) {
-                    car.uTurn();
-                }
-
-                frame.drawPanel.moveit(car, x, y);
-                // repaint() calls the paintComponent method of the panel
-
-            }
-            frame.drawPanel.repaint();
+            model.update();
+            updateableView.repaint();
         }
     }
 
     // Calls the gas method for each car once
     void gas(int amount) {
-        double gas = ((double) amount) / 100;
-        for (Car car : cars) {
-            car.gas(gas);
-        }
+        model.gas(amount);
     }
 
     // Calls the brake method for each car once
     public void brake(int amount) {
-        double brake = ((double) amount) / 100;
-        for (Car car : cars) {
-            car.brake(brake);
-        }
+        model.brake(amount);
     }
 }
