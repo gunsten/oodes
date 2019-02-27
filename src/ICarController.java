71c9273085abd3d6a20d83143c781d46051698da
gunsten <gunnar.stenlund@gmail.com>
2019-02-27 15:23:10 +0100

    Adds ICC interface
    Renames IModel methods

--- src/ICarController.java
+++ src/ICarController.java
@@ -1,11 +1,9 @@
-public interface IModel {
-    void update();
+public interface ICarController {
     void gas();
     void brake();
     void startCars();
     void stopCars();
     void liftBeds();
     void lowerBeds();
-    void turbosOn();
-    void turbosOff();
+    void turbos(boolean on);
 }
