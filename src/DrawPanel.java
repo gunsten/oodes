71ff3df32487bca78e21d50f1d8b62c47eff034d
gunsten <gunnar.stenlund@gmail.com>
2019-02-27 15:49:20 +0100

    Implements refactored DP

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -13,66 +13,26 @@ import javax.swing.*;
 public class DrawPanel extends JPanel{
 
     // Images associated with each car modelled
-    Map<Car, BufferedImage> carImages;
-    BufferedImage volvoImage, saabImage, scaniaImage;
-    // Points associated with each car modelled
-    Map<Car, Point> carPoints;
+    Map<Locateable, BufferedImage> carImages;
 
-    //All modelled cars
-    List<Car> cars;
-
-    void moveit(Car car, int x, int y){
-        carPoints.get(car).x = x;
-        carPoints.get(car).y = y;
-    }
-
-    void addCar(Car car, int x, int y) {
-        if (car instanceof Volvo240)
-            carImages.put(car, volvoImage);
-        else if (car instanceof Saab95)
-            carImages.put(car, saabImage);
-        else if (car instanceof Scania)
-            carImages.put(car, scaniaImage);
-        else
-            throw new RuntimeException("No image associated to " + car.getClass().getName());
-        cars.add(car);
-        carPoints.put(car, new Point(x, y));
-    }
+    ReadableModel<Locateable> model;
 
     // Initializes the panel and reads the images
-    public DrawPanel(int x, int y) {
+    public DrawPanel(ReadableModel<Locateable> model, Map<Locateable, BufferedImage> carImages, int x, int y) {
+        this.carImages = carImages;
+        this.model = model;
+
         this.setDoubleBuffered(true);
         this.setPreferredSize(new Dimension(x, y));
         this.setBackground(Color.green);
-
-        cars = new ArrayList<>();
-        carImages = new HashMap<>();
-        carPoints = new HashMap<>();
-
-        // Print an error message in case file is not found with a try/catch block
-        try {
-            // You can remove the "pics" part if running outside of IntelliJ and
-            // everything is in the same main folder.
-            // volvoImage = ImageIO.read(new File("Volvo240.jpg"));
-
-            // Rememember to rightclick src New -> Package -> name: pics -> MOVE *.jpg to pics.
-            // if you are starting in IntelliJ.
-            volvoImage = ImageIO.read(DrawPanel.class.getResourceAsStream("Volvo240.jpg"));
-            saabImage = ImageIO.read(DrawPanel.class.getResourceAsStream("Saab95.jpg"));
-            scaniaImage = ImageIO.read(DrawPanel.class.getResourceAsStream("Scania.jpg"));
-        } catch (IOException ex)
-        {
-            ex.printStackTrace();
-        }
-
     }
 
     // This method is called each time the panel updates/refreshes/repaints itself
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        for (Car car : cars) {
-            g.drawImage(carImages.get(car), carPoints.get(car).x, carPoints.get(car).y, null);
+        for (Locateable car : model.get()) {
+            g.drawImage(carImages.get(car), (int) car.getPosition().x, (int) car.getPosition().y, null);
         }
     }
 }
