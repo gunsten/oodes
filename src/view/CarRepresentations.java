72477f519479999a2d0564f34f42e1874cbcf383
gunsten <gunnar.stenlund@gmail.com>
2019-03-01 17:27:28 +0100

    Applies Singleton pattern on CR

--- src/view/CarRepresentations.java
+++ src/view/CarRepresentations.java
@@ -13,36 +13,41 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CarRepresentations {
-    Map<Class<? extends Locateable>, Image> map;
+    private Map<Class<? extends Locateable>, Image> map;
+    private static CarRepresentations carRepresentations;
 
     private CarRepresentations() {
         map = new HashMap<>();
     }
 
-    public static CarRepresentations getDefaultCarRepresentations() {
-        CarRepresentations cr = new CarRepresentations();
-        BufferedImage volvoImage = null, saabImage = null, scaniaImage = null;
-        // Instance of this class
-        //controller.ICarController cc = new controller.CarController();
-
-        try {
-            // You can remove the "pics" part if running outside of IntelliJ and
-            // everything is in the same main folder.
-            // volvoImage = ImageIO.read(new File("model.Volvo240.jpg"));
-
-            // Rememember to rightclick src New -> Package -> name: pics -> MOVE *.jpg to pics.
-            // if you are starting in IntelliJ.
-            volvoImage = ImageIO.read(CarRepresentations.class.getResourceAsStream("Volvo240.jpg"));
-            saabImage = ImageIO.read(CarRepresentations.class.getResourceAsStream("Saab95.jpg"));
-            scaniaImage = ImageIO.read(CarRepresentations.class.getResourceAsStream("Scania.jpg"));
-        } catch (IOException ex)
-        {
-            ex.printStackTrace();
+    public static CarRepresentations getCarRepresentations() {
+        if (carRepresentations != null) {
+            return carRepresentations;
         }
-        cr.addRepresentation(Volvo240.class, volvoImage);
-        cr.addRepresentation(Saab95.class, saabImage);
-        cr.addRepresentation(Scania.class, scaniaImage);
-        return cr;
+        else {
+            carRepresentations = new CarRepresentations();
+            BufferedImage volvoImage = null, saabImage = null, scaniaImage = null;
+            // Instance of this class
+            //controller.ICarController cc = new controller.CarController();
+
+            try {
+                // You can remove the "pics" part if running outside of IntelliJ and
+                // everything is in the same main folder.
+                // volvoImage = ImageIO.read(new File("model.Volvo240.jpg"));
+
+                // Rememember to rightclick src New -> Package -> name: pics -> MOVE *.jpg to pics.
+                // if you are starting in IntelliJ.
+                volvoImage = ImageIO.read(CarRepresentations.class.getResourceAsStream("Volvo240.jpg"));
+                saabImage = ImageIO.read(CarRepresentations.class.getResourceAsStream("Saab95.jpg"));
+                scaniaImage = ImageIO.read(CarRepresentations.class.getResourceAsStream("Scania.jpg"));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            carRepresentations.addRepresentation(Volvo240.class, volvoImage);
+            carRepresentations.addRepresentation(Saab95.class, saabImage);
+            carRepresentations.addRepresentation(Scania.class, scaniaImage);
+            return carRepresentations;
+        }
     }
 
     public void addRepresentation(Class<? extends Locateable> locatable_class, Image image) {
