f7b3cdb64c8d34ab0062bfe8c69dd29b3f999353
gunsten <gunnar.stenlund@gmail.com>
2019-03-01 17:55:54 +0100

    Removes junk

--- src/Main.java
+++ src/Main.java
@@ -19,8 +19,8 @@ public class Main {
 
         CarController cc = new CarController(drawPanel, model);
 
-        // Start a new view and send a reference of self
-        CarView frame = new CarView("CarSim 1.0", cc, drawPanel);
+        // Start a new view
+        new CarView("CarSim 1.0", cc, drawPanel);
 
 
         // Start the timer
